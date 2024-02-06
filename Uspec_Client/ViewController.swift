f8788ac54a0d3a816a00a394205694698487d99b
haram22 <kimhr9889@handong.ac.kr>
2024-02-06 17:28:46 +0900

    [Init] initial setting

---
+++ Uspec_Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uspec_Client
+//
+//  Created by 김하람 on 2/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
