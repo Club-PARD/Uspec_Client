7e3822f34bbf14c6242d80ae7bb275703cfa184a
haram22 <kimhr9889@handong.ac.kr>
2024-02-12 01:25:28 +0900

    login button

--- Uspec_Client/Sources/Scene/Login/LoginViewController.swift
+++ Uspec_Client/Sources/Scene/Login/LoginViewController.swift
@@ -10,6 +10,22 @@ import AuthenticationServices
 
 class LoginViewController: UIViewController {
     override func viewDidLoad() {
-        viewDidLoad()
+        super.viewDidLoad()
+        setUi()
+    }
+    
+    private func setUi() {
+        let loginButton = UIButton()
+        loginButton.setTitle("login with apple", for: .normal)
+        loginButton.backgroundColor = .systemBlue
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(loginButton)
+        NSLayoutConstraint.activate([
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginButton.widthAnchor.constraint(equalToConstant: 200),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 }
