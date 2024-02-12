1857c9c73cbba6d13f835e552257d1034bdb2d2c
haram22 <kimhr9889@handong.ac.kr>
2024-02-13 02:31:53 +0900

    [Feat] profile 입력 페이지 네비게이션 바 커스텀

--- Uspec_Client/Sources/Scene/Profile/ProfileViewController.swift
+++ Uspec_Client/Sources/Scene/Profile/ProfileViewController.swift
@@ -11,7 +11,16 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonTapped))
+        backButton.tintColor = UIColor(resource: .textBlack)
+        let textButton = UIBarButtonItem(title: "회원가입", style: .plain, target: self, action: #selector(backButtonTapped))
+        textButton.tintColor = UIColor(resource: .textBlack)
+        self.navigationItem.leftBarButtonItems = [backButton, textButton]
         setupUI()
+    }
+    
+    @objc func backButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
     }
     
     private func setupUI() {
