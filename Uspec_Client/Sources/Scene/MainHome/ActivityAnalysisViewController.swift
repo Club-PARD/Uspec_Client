2afc0ed02652bb87344d1e4c5a0d226af185aadc
haram22 <kimhr9889@handong.ac.kr>
2024-02-17 01:53:23 +0900

    [Feat] navigate to activity

---
+++ Uspec_Client/Sources/Scene/MainHome/ActivityAnalysisViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityAnalysisViewController.swift
+//  Uspec_Client
+//
+//  Created by 김하람 on 2/17/24.
+//
+
+import UIKit
+
+class ActivityAnalysisViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+    
+}
